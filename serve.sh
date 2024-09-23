a5f16028a4b893e523b4227e3585d3ae37c8b19b
Ahmet Sait <git@ahmetsait.com>
2024-09-23 03:50:11 +0300

    Local https prevents testing on phone over LAN so not worth it
    Not to mention the fact that incompetent Jekyll devs can't make `--livereload` work with https

--- serve.sh
+++ serve.sh
@@ -4,20 +4,20 @@ set -e
 
 ./build.sh --watch --no-jekyll
 
-crt="localhost.pem"
-key="localhost-key.pem"
-
-if [[ ! -f "$crt" || ! -f "$key" ]]; then
-	if command -v mkcert > /dev/null; then
-		mkcert -install
-		mkcert -cert-file "$crt" -key-file "$key" localhost 127.0.0.1 "::1" 0.0.0.0 192.168.1.100
-	fi
-fi
-
-if [[ -f "$crt" && -f "$key" ]]; then
-	# Can't serve live reload script over https
-	# https://github.com/jekyll/jekyll/issues/9495
-	bundler exec jekyll serve --ssl-cert "$crt" --ssl-key "$key" "$@"
-else
+#crt="localhost.pem"
+#key="localhost-key.pem"
+#
+#if [[ ! -f "$crt" || ! -f "$key" ]]; then
+#	if command -v mkcert > /dev/null; then
+#		mkcert -install
+#		mkcert -cert-file "$crt" -key-file "$key" localhost 127.0.0.1 "::1" 0.0.0.0 192.168.1.100
+#	fi
+#fi
+#
+#if [[ -f "$crt" && -f "$key" ]]; then
+#	# Can't serve live reload script over https
+#	# https://github.com/jekyll/jekyll/issues/9495
+#	bundler exec jekyll serve --ssl-cert "$crt" --ssl-key "$key" "$@"
+#else
 	bundler exec jekyll serve --livereload "$@"
-fi
+#fi
