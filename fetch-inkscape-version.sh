3a5911eb11892541951a95cb1c727fe6e0415a79
Ahmet Sait <git@ahmetsait.com>
2024-09-23 05:56:20 +0300

    AppImage binaries need FUSE which is not present in GitHub runners

--- fetch-inkscape-version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-inkscape_url="$(curl -sLI 'https://inkscape.org/release/' -o /dev/null -w '%{url_effective}')"
-inkscape_version="$inkscape_url"
-inkscape_version="${inkscape_version%/}"
-inkscape_version="${inkscape_version##*/}"
-printf "%s" "$inkscape_version"
--- fetch-magick-etag.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sLI 'https://imagemagick.org/archive/binaries/magick' | perl -nE 'm{^ETag:\s*(.+?(?=\r\n))}i && printf($1)'
